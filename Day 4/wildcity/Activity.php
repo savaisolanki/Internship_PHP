514436bba13f42b34bbbf9187e7d871b00297468
Savai Solanki <69721607+savaisolanki@users.noreply.github.com>
2021-05-31 11:04:44 +0530

    Day_4
    Day_4

---
+++ Day 4/wildcity/Activity.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Wild City - Tony Pires</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="container">
+<?php
+include 'header.html';
+?>
+<div id="mainContent">
+<?php
+
+include 'archive.html';
+include 'cato.html';
+?> 
+<br class="clear" />     
+</div>
+<?php
+include 'footer.html'; 
+?>
+</div>
+</body>
+</html>
